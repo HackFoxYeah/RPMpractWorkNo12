f039df2ca385369da76be829c13cfa60decc96e3
Короче, Максон <102228515+HackFoxYeah@users.noreply.github.com>
2022-12-03 18:18:22 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+
+namespace practicalWorkNo12
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+            => InitializeComponent();
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+            => Close();
+
+        private void BtnDoATask_Click(object sender, RoutedEventArgs e)
+        {
+            SupClass.TaskFunc(tbInput, tbResult);
+        }
+
+        private void ShowInfo_Click(object sender, RoutedEventArgs e)
+            => MessageBox.Show("1) Даны стороны прямоугольника a и b. Найти его площадь периметр\n" +
+                               "2) Дано трехзначное число. Вывести число, полученное при прочтении исходного числа справа налево.");
+
+        private void TbInput_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+            => BtnDoATask.IsEnabled = true;
+    }
+}
---
+++ SupClass.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace practicalWorkNo12
+{
+    internal class SupClass
+    {
+        /// <summary>
+        /// Выполняет задание
+        /// </summary>
+        /// <param name="input">ТекстБокс с входными данными</param>
+        /// <param name="result">ТекстБокс с выходными даннымим</param>
+        public static void TaskFunc(TextBox input, TextBox result)
+        {
+            if (input.Text == string.Empty)
+            {
+                MessageBox.Show("Ввод одного числа - поиск его зеркальной версии, ввод двух чисел - поиск площади и периметра прямоугольника со сторонами a и b, равными этим числам.", "Введено неверное количество чисел!");
+                return;
+            }
+
+            string[] data = input.Text.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (data.Length == 0 || data.Length > 2)
+            {
+                MessageBox.Show("Ввод одного числа - поиск его зеркальной версии, ввод двух чисел - поиск площади и периметра прямоугольника со сторонами a и b, равными этим числам.", "Введено неверное количество чисел!");
+                return;
+            }
+            else if (data.Length == 1)
+            {
+                uint value = uint.Parse(data[0]),
+                     valueCopy = value,
+                     temp = default;
+
+                if (value == 0)
+                {
+                    result.Text = $"Число 0, при чтении справа налево, будет выглядеть как 0";
+                    return;
+                }
+
+                while (value != 0)
+                {
+                    temp = temp * 10 + value % 10;
+
+                    value /= 10;
+                }
+
+                result.Text = $"Число {valueCopy}, при чтении справа налево, будет выглядеть как {temp}";
+            }
+            else if (data.Length == 2)
+            {
+                uint aSide = uint.Parse(data[0]),
+                     bSide = uint.Parse(data[1]);
+
+                if (aSide == 0 && bSide == 0)
+                {
+                    result.Text = $"Площадь прямоугольника со сторонами 0 и 0 навна: 0\nЕго периметр равен: 0";
+                    return;
+                }
+
+                result.Text = $"Площадь прямоугольника со сторонами {aSide} и {bSide} равна: {aSide * bSide}\nЕго периметр равен: {(aSide + bSide) * 2}";
+            }
+        }
+    }
+}
